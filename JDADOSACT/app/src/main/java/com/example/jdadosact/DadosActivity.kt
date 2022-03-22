8b2023fe50dde9ba3b861416bf6a39a5cafc44f2
Daiane Ferreira <daianeferreira0@gmail.com>
2022-03-21 21:30:54 -0300

    Atualização da imagem para Dados

--- JDADOSACT/app/src/main/java/com/example/jdadosact/DadosActivity.kt
+++ JDADOSACT/app/src/main/java/com/example/jdadosact/DadosActivity.kt
@@ -2,10 +2,66 @@ package com.example.jdadosact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class DadosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dados)
+
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val imageDados = findViewById<ImageView>(R.id.imageDados)
+        val buttonjogar = findViewById<Button>(R.id.buttonjogar)
+        val buttonvoltar = findViewById<Button>(R.id.buttonvoltar)
+
+        val ident = intent.getIntExtra("TIPO", 0)
+
+
+        buttonjogar.setOnClickListener {
+            jogar(ident)
+        }
+
+        buttonvoltar.setOnClickListener {
+            finish()
+        }
+
+        }
+        fun jogar(lados:Int){
+
+            val resul = (1..lados).random()
+
+            val imageResource = NumD20(resul)
+            val imageNum = findViewById<ImageView>(R.id.imageDados)
+            imageNum.setImageResource(imageResource)}
+
+    private fun NumD20(valor: Int): Int {
+        return when (valor) {
+
+        1 -> R.drawable.num1
+        2 -> R.drawable.num2
+        3 -> R.drawable.num3
+        4 -> R.drawable.num4
+        5 -> R.drawable.num5
+        6 -> R.drawable.num6
+        7 -> R.drawable.num7
+        8 -> R.drawable.num8
+        9 -> R.drawable.num9
+        10 -> R.drawable.num10
+        11 -> R.drawable.num11
+        12 -> R.drawable.num12
+        13 -> R.drawable.num13
+        14 -> R.drawable.num14
+        15 -> R.drawable.num15
+        16 -> R.drawable.num16
+        17 -> R.drawable.num17
+        18 -> R.drawable.num18
+        19 -> R.drawable.num19
+        else -> R.drawable.num20
+    }
     }
 }
+
+
--- JDADOSACT/app/src/main/java/com/example/jdadosact/MainActivity.kt
+++ JDADOSACT/app/src/main/java/com/example/jdadosact/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.jdadosact
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonD6 = findViewById<Button>(R.id.buttonD6)
+        val buttonD12 = findViewById<Button>(R.id.button12)
+        val buttonD20 = findViewById<Button>(R.id.button20)
+
+        val intentDados = Intent(this, DadosActivity::class.java)
+
+        buttonD6.setOnClickListener {
+            intentDados.putExtra("TIPO", 6)
+            startActivity(intentDados)
+        }
+
+        buttonD12.setOnClickListener {
+            intentDados.putExtra("TIPO", 12)
+            startActivity(intentDados)
+
+        }
+
+        buttonD20.setOnClickListener {
+            intentDados.putExtra("TIPO", 20)
+            startActivity(intentDados)
+        }
+
     }
 }
