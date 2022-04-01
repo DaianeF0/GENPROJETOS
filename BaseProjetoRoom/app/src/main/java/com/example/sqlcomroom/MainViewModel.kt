8daa63d8460c189d21ff70c83f386f1f7bebd826
Daiane Ferreira <daianeferreira0@gmail.com>
2022-04-01 14:49:55 -0300

    Room

---
+++ BaseProjetoRoom/app/src/main/java/com/example/sqlcomroom/MainViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.sqlcomroom
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sqlcomroom.data.UserDataBase
+import com.example.sqlcomroom.data.UserRepository
+import com.example.sqlcomroom.model.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel (context: Context?) : ViewModel () {
+
+    val lerTodosOsDados : LiveData<List<User>>
+    val repository : UserRepository
+
+    init {
+        val userDao = UserDataBase.getDatabase(context!!).userDao()
+        repository = UserRepository(userDao)
+        lerTodosOsDados = repository.lerTodosOsDados
+    }
+
+    fun addUser (user: User){
+
+        viewModelScope.launch ( Dispatchers.IO) {
+            repository.addUser(user)
+        }
+    }
+
+}
