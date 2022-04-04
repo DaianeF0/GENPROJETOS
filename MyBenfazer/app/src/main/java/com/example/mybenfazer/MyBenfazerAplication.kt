037cbae873383addad297df6844cecf38892ae5a
DaianeF0 <99733351+DaianeF0@users.noreply.github.com>
2022-04-04 09:50:32 -0300

    Delete MyBenfazer directory

--- MyBenfazer/app/src/main/java/com/example/mybenfazer/MyBenfazerAplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mybenfazer
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-
-@HiltAndroidApp
-class MyBenfazerAplication : Application() {
-}
--- MyBenfazer/app/src/main/java/com/example/mybenfazer/util/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.generation.benfazerproject.util
-
-object Constants {
-    const val BASE_URL = "https://mobileecoceum.herokuapp.com/"
-}
